4b6193a30f13259ad4f514644ae822ec274bc1d8
Your Name <you@example.com>
2024-11-13 10:50:07 +0100

    correctif

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,26 +1,15 @@
-/**
- * Returns a string expressing preference for const.
- * @returns {string} A constant string.
- */
+// 0-constants.js
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.'; // Utilisation de const
   return task;
 }
 
-/**
- * Returns a string to be appended.
- * @returns {string} A constant string.
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * Combines strings using let for a mutable variable.
- * @returns {string} The combined string.
- */
 export function taskNext() {
-  let combination = 'But sometimes let';
+  let combination = 'But sometimes let'; // Utilisation de let
   combination += getLast();
 
   return combination;
