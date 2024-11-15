cdaa9d0d137323e417c2364a9ec9400764e298d9
Your Name <you@example.com>
2024-11-15 11:03:59 +0100

    correctif2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,28 +1,29 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    // Vérification des types des attributs
+    // Verify and set name
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = name;
+
+    // Verify and set length
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
+
+    // Verify and set students
     if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-
-    // Assignation des attributs avec un underscore
-    this._name = name;
-    this._length = length;
     this._students = students;
   }
 
-  // Getter pour name
+  // Getter and setter for name
   get name() {
     return this._name;
   }
 
-  // Setter pour name
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -30,12 +31,11 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
-  // Getter pour length
+  // Getter and setter for length
   get length() {
     return this._length;
   }
 
-  // Setter pour length
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -43,12 +43,11 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-  // Getter pour students
+  // Getter and setter for students
   get students() {
     return this._students;
   }
 
-  // Setter pour students
   set students(value) {
     if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
