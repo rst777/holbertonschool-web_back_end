35eb507a7aa995c83a52d9edb1bbd5a2594219d4
Your Name <you@example.com>
2024-11-15 10:26:23 +0100

    0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,13 @@
-// 0-constants.js
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Utilisation de const
+  const task = 'I prefer const when I can.';
   return task;
 }
-
 export function getLast() {
   return ' is okay';
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let'; // Utilisation de let
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
