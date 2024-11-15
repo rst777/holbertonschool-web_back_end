c9eeb5e6040a47845f4e2ad62c4af3be6f50d527
Your Name <you@example.com>
2024-11-15 10:27:52 +0100

    correctif5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,11 +1,3 @@
-/**
- * Concatenates two arrays and each character of a string into a single array.
- *
- * @param {Array} array1 - The first array to concatenate.
- * @param {Array} array2 - The second array to concatenate.
- * @param {string} string - The string to split into individual characters and concatenate.
- * @returns {Array} A new array containing all elements from array1, array2, and each character of the string.
- */
 export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
