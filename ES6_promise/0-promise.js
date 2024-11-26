002425bf60a8e4cfeda3003b096db3057f263d3b
Your Name <you@example.com>
2024-11-26 14:26:46 +0100

    0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // On peut ici ajouter une logique asynchrone si nécessaire.
-    resolve(); // La promesse est résolue avec succès.
   });
 }
