9eb3ee346b4390ae4aa1e40dc928910f696a412a
Your Name <you@example.com>
2024-12-04 12:39:57 +0100

    correctif

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to compute the sum of a list of floats.
+"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Compute and return the sum of a list of floats.
+
+    Args:
+        input_list (List[float]): A list of float numbers.
+
+    Returns:
+        float: The sum of the float numbers in the list.
+    """
     return sum(input_list)
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
-from typing import List, Union
+"""
+This module provides a function to compute the sum of
+a list containing integers and floats.
+"""
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    return float(sum(mxd_lst))
+    """
+    Compute and return the sum of a list of integers and floats.
+
+    Args:
+        mxd_lst (List[Union[int, float]]):
+        A list containing integers and floats.
+
+    Returns:
+        float: The sum of the numbers in the list as a float.
+    """
+    return sum(mxd_lst)
