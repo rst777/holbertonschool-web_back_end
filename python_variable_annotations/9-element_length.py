053e568db6e0443326f0cbb699c29ca413bca17e
Your Name <you@example.com>
2024-12-04 12:49:27 +0100

    correctif

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -8,12 +8,15 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Returns a list of tuples, where each tuple contains an element and its length.
+    Returns a list of tuples, where each tuple
+    contains an element and its length.
 
     Args:
-        lst (Iterable[Sequence]): A list (or any iterable) of sequences (like strings, lists, etc.).
+        lst (Iterable[Sequence]): A list (or any iterable)
+        of sequences (like strings, lists, etc.).
 
     Returns:
-        List[Tuple[Sequence, int]]: A list of tuples, each containing an element and its length.
+        List[Tuple[Sequence, int]]: A list of tuples,
+        each containing an element and its length.
     """
     return [(i, len(i)) for i in lst]
