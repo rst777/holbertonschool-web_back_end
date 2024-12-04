0da10a7e2515d73ecc6bdb317994a6f2ba2c7c16
Your Name <you@example.com>
2024-12-04 13:33:06 +0100

    correctif

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,18 +2,19 @@
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Attends de manière asynchrone un délai aléatoire compris
-    entre 0 et max_delay (inclus).
-    La fonction retourne le temps d'attente.
+    Coroutine asynchrone qui attend un délai aléatoire entre 0 et max_delay
+    (inclus) et retourne ce délai.
 
     Arguments :
-        max_delay (int) : Le délai maximum en secondes. Par défaut, 10.
+        max_delay (int) : Le délai maximal (en secondes) pour attendre
+        (par défaut 10).
 
     Retour :
-        float : Le délai réel en secondes.
+        float : Le délai aléatoire obtenu entre 0 et max_delay.
     """
-    delay = random.uniform(0, max_delay)  # Génère un nombre flottant aléatoire entre 0 et max_delay
-    await asyncio.sleep(delay)  # Attend de manière asynchrone le délai généré
-    return delay  # Retourne le délai
+    delay = random.uniform(0, max_delay)  # Génère un délai aléatoire
+    await asyncio.sleep(delay)  # Attend ce délai
+    return delay  # Retourne le délai généré
