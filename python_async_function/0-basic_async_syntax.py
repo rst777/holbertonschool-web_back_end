a369c6494c76308c0e9e2cd7323732041cadee86
Your Name <you@example.com>
2024-12-04 14:31:59 +0100

    0-basic_async_syntax.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
+"""
+Module for generating random asynchronous delays.
+"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Wait for a random delay between 0 and max_delay seconds.
+    Generate a random delay between 0 and max_delay seconds.
 
     Args:
         max_delay (int): Maximum delay in seconds. Defaults to 10.
 
     Returns:
-        float: Actual random delay
+        float: Random delay
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
